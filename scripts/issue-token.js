48f16bef3eb772921709b9dedb5e6167d9cbe4a7
lsromao <lucas.souzaromao@gmai.com>
2021-09-25 19:59:16 +0200

    Completed tutorial

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,8 +1,10 @@
 const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = async function(callback){
-    console.log('Tokens issued.')
+module.exports = async function(callback) {
+    
+  let tokenFarm = await TokenFarm.deployed()
+  await tokenFarm.issueTokens()
 
-    callback()
-
+  console.log("Tokens issued!")
+  callback()
 }
